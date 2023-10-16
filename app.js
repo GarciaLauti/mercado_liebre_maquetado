fd3b730f872a1e37c4d7fb87095c9ed60fb600d7
garcialauti <lautarogarcia.-@hotmail.com>
2023-10-16 18:42:07 -0300

    subiendo ejercicio

---
+++ app.js
@@ -0,0 +1,9 @@
+const { log } = require('console')
+const express =require('express')
+const path = require('path')
+const port = 3030
+const app= express()
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views','home.html')) )
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(port, console.log(`server run in http://localhost:${port}`))
